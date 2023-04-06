2dc8c4cdf35ff3039620bcac9e1ca10ee28f68de
Isaac Mann <isaacplmann@gmail.com>
2023-04-06 11:29:19 -0400

    lab5

--- apps/api-e2e/jest.config.ts
+++ apps/api-e2e/jest.config.ts
@@ -1,17 +1,19 @@
 /* eslint-disable */
 export default {
-  displayName: "api-e2e",
-  preset: "../..//jest.preset.js",
+  displayName: 'api-e2e',
+  preset: '../../jest.preset.js',
   globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.spec.json",
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
     },
   },
-  setupFiles: ["<rootDir>/src/test-setup.ts"],
-  testEnvironment: "node",
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
   transform: {
-    "^.+\\.[tj]s$": "ts-jest",
+    '^.+\\.[tj]s$': 'ts-jest',
   },
-  moduleFileExtensions: ["ts", "js", "html"],
-  coverageDirectory: "../..//coverage/api-e2e",
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/api-e2e',
 };
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,23 +1,28 @@
 /**
-* This is not a production server yet!
-* This is only a minimal backend to get started.
-*/
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
 
 import express from 'express';
+import { doAuth } from '@bg-hoard/api/auth';
 import { getAllGames, getGame } from './app/games.repository';
 
 const app = express();
 
 app.get('/api/games', (req, res) => {
- res.send(getAllGames());
+  res.send(getAllGames());
 });
 
 app.get('/api/games/:id', (req, res) => {
- return res.send(getGame(req.params.id));
+  return res.send(getGame(req.params.id));
+});
+
+app.post('/api/auth', (req, res) => {
+  res.send(doAuth());
 });
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
- console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
